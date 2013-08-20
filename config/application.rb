51ec0bbd3e2f357642c37375661a9be8e173e441
Vitor Baptista <vitor@vitorbaptista.com>
2013-08-20 13:06:12 -0300

    Adiciona link para curtir no Facebook

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module Inesc
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_dispatch.default_headers['X-Frame-Options'] = 'ALLOW-FROM //www.facebook.com'
   end
 end
